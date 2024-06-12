9673a2572175ddbc037efa570b534d62fbce9aef
Naahas <150798303+naahas@users.noreply.github.com>
2024-06-12 15:39:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,17 @@ client.on('ready', () => {
 
 
 function launch() {
-   
-    summonTask()
+    const channel = client.channels.cache.get('785917648693100605')
+    channel.send('$daily')
 
+    setTimeout(() => {
+        channel.send('$dk')
+    }, 2000);
+   
+    setTimeout(() => {
+        summonTask(0 , 0 , 10)
+    }, 1000);
+   
 
     setInterval(() => {
         console.log('alive')
@@ -27,45 +35,74 @@ function launch() {
 
 
     setInterval(() => {
-        summonTask()
+        summonTask(0 , 0 , 10)
     }, 3600000);
+
+
+    setInterval(() => {
+        channel.send('$daily')
+    }, 73800000);
     
 
 }
 
-function summonTask() {
+function summonTask(auth , auth2 , roll) {
     var val = 0
-      const channel = client.channels.cache.get('785917648693100605');    
-    
-    var summon = setInterval(() => {
-        channel.send(`$ma`)
+    var left = roll
+    var claimed = false
+    const channel = client.channels.cache.get('785917648693100605');    
+    // var rn; 
+    // const cn = ['mario' , 'shrek' , 'kacchan' , 'meruem' , 'son gohan' , 'ash ketchum' , 'majin buu']
 
-        setTimeout(() => {
-            channel.messages.fetch({ limit: 1 }).then(messages => {
-                const last = messages.first();
-                // console.log(last)
+    if(left > 0) {
+        var summon = setInterval(() => {
+            // rn = Math.floor(Math.random() * 7);
+            channel.send(`$ma`)
+            left--
 
-                if(last.embeds.length > 0 && last.author.username == 'Mudae' && last.components[0].components.length > 0) {
-                    const btn = last.components[0].components[0]
-                    checkWish(last.embeds[0].author.name)
-                    .then(found => {
-                        if (found == true) {
-                            setTimeout(() => {
-                                btn.click(last);
-                            }, 700);
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Une erreur est survenue lors de la vérification du souhait :', error);
-                    });
-                }
+            // START ROLLS AND CLAIM SPECIFIC CHARACTER
+            setTimeout(() => {
+                channel.messages.fetch({ limit: 1 }).then(messages => {
+                    const last = messages.first();
+                    // console.log(last)
 
-            }).catch(console.error);
-        }, 800);
-        val++
-        if(val == 10) clearInterval(summon)
+                    if(last.embeds.length > 0 && last.author.username == 'Mudae' && last.components[0].components.length > 0) {
+                        const btn = last.components[0].components[0]
+                        checkWish(last.embeds[0].author.name)
+                        .then(found => {
+                            if (found == true) {
+                                claimed = true
+                                setTimeout(() => {
+                                    btn.click(last);
+                                    if(auth2 != 1) {
+                                        clearInterval(summon)
+                                        checkRT(left)
+                                    } 
+                                }, 700);
+                            }
+                        })
+                        .catch(error => {
+                            console.error('Une erreur est survenue lors de la vérification du souhait :', error);
+                        });
+                    }
 
-    }, 3500);
+                }).catch(console.error);
+            }, 800);
+            val++
+
+            // CHECK IF PLAYER CAN DAILY AFTER FINISHING ROLLS
+            if(val == roll) {
+                clearInterval(summon)
+                setTimeout(() => {
+                    if(auth != 1 && claimed != true) checkDaily(roll)
+                }, 2000);
+                
+            }
+
+        }, 3500);
+
+       
+    } 
 
 }
 
@@ -94,6 +131,64 @@ async function checkWish(character) {
         console.error('Une erreur est survenue :', error);
     }
 }
+
+
+function checkRT(left) {
+    const channel = client.channels.cache.get('785917648693100605');  
+    setTimeout(() => {      
+        channel.send('$mu')
+    }, 1000);
+    
+
+    setTimeout(() => {
+        channel.messages.fetch({ limit: 1 }).then(messages => {
+            const last = messages.first();
+            // var tt = "nahass, temps restant avant de pouvoir vous remarier : **2h 02** min."
+    
+            if(last.author.username == 'Mudae') {
+                var regex = /(\d{1,2})h/;
+                var match = last.content.match(regex);
+                
+                if(match) {
+                    channel.send('$rt')
+
+                    setTimeout(() => {
+                        summonTask(0 , 1 , left)
+                    }, 1000);
+                }
+            }
+    
+        }).catch(console.error);
+    }, 2400);
+}
+
+function checkDaily(roll) {
+    const channel = client.channels.cache.get('785917648693100605');    
+    channel.send('$mu')
+
+    setTimeout(() => {
+        channel.messages.fetch({ limit: 1 }).then(messages => {
+            const last = messages.first();
+            // console.log(last)
+    
+            if(last.author.username == 'Mudae') {
+               var cv = last.content 
+
+               // RESET ROLL IF DAILY ALLOWED AND THEN SUMMON
+               if(cv.includes('vous __pouvez__')) {
+                  channel.send('$rolls')
+
+                  setTimeout(() => {
+                    summonTask(1 , 0 , roll)
+                  }, 1500);
+               }
+            }
+    
+        }).catch(console.error);
+    }, 1000);
+    
+}
+
 
 
 
